2c928e7127178ebfdcd9424c19ab98743b214341
ghefira0865 <ghefirahidayahtullah@students.amikom.ac.id>
2024-12-10 21:53:13 +0700

    Update web.php

--- routes/web.php
+++ routes/web.php
@@ -132,6 +132,7 @@ Route::get('/desawisata', [PageController::class, 'desawisata']);
 // Route untuk login
 Route::get('/login', [AuthController::class, 'login'])->name('login');
 Route::post('/login', [AuthController::class, 'authenticate'])->name('authenticate');
+Route::post('/logout', [AuthController::class, 'logout'])->name('logout');
 Route::get('/register', [AuthController::class, 'register'])->name('register');
 Route::post('/register', [AuthController::class, 'registerAdmin'])->name('register.process');
 
