b12def96baea615046c610043cb6818c43f33b0f
0826Divalathifah <divalathifahsetiyawati@students.amikom.ac.id>
2024-12-10 22:43:41 +0700

    Update Preneur.php

--- app/Models/Preneur.php
+++ app/Models/Preneur.php
@@ -24,31 +24,9 @@ class Preneur extends Model
         'nomor_whatsapp',
         'deskripsi',
         'foto_card',
-        'foto_produk', // Untuk menyimpan path gambar produk dalam format JSON
+        'foto_slider', // Untuk menyimpan path gambar produk dalam format JSON
     ];
+    
+   
 
-    // Mutator untuk menyimpan array sebagai JSON
-    protected function setFotoProdukAttribute($value)
-    {
-        $this->attributes['foto_produk'] = json_encode($value);
-    }
-
-    // Accessor untuk mendapatkan data JSON sebagai array
-    public function getFotoProdukAttribute($value)
-    {
-        return is_array(json_decode($value, true)) ? json_decode($value, true) : [];
-    }
-
-    // Accessor untuk memformat harga
-    public function getHargaProdukAttribute($value)
-    {
-        return 'Rp ' . number_format($value, 0, ',', '.');
-    }
-
-    public function setHargaProdukAttribute($value)
-    {
-        $this->attributes['harga_produk'] = str_replace('.', '', $value);
-    }
-
-    // Sesuaikan foreign key 'id_produk' menjadi 'id'
 }
