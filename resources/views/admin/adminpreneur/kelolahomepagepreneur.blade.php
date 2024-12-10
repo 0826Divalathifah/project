c4d9082c7b2a6ccef5b97b47f28468f58f922efe
0826Divalathifah <divalathifahsetiyawati@students.amikom.ac.id>
2024-12-10 23:02:42 +0700

    Update kelolahomepagepreneur.blade.php

--- resources/views/admin/adminpreneur/kelolahomepagepreneur.blade.php
+++ resources/views/admin/adminpreneur/kelolahomepagepreneur.blade.php
@@ -4,7 +4,7 @@
     <!-- Required meta tags -->
     <meta charset="utf-8">
     <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
-    <title>Kelola Homepage Preneur</title>
+    <title>Kelola Home Page Preneur</title>
     <!-- plugins:css -->
     <link rel="stylesheet" href="{{ asset('admin/assets/vendors/feather/feather.css') }}">
     <link rel="stylesheet" href="{{ asset('admin/assets/vendors/ti-icons/css/themify-icons.css') }}">
@@ -30,87 +30,87 @@
   <body>
     
   <div class="container-scroller">
-<!-- partial:../../partials/_navbar.html -->
-<nav class="navbar col-lg-12 col-12 p-0 fixed-top d-flex flex-row">
-        <div class="text-center navbar-brand-wrapper d-flex align-items-center justify-content-start">
-        <a class="navbar-brand brand-logo me-5" href="{{ url ('/penjual') }}" >
-            <img src="{{ asset('themewagon/img/logo/logo_header.png') }}" alt="Logo Kabupaten Sleman" style="width: 110 px; height: 52px;">
-          </a>
-          <a class="navbar-brand brand-logo-mini" href="{{ url('/penjual') }}">
-            <img src="{{ asset('themewagon/img/logo/logo kabupaten sleman.png') }}"  alt="Logo Kabupaten Sleman" style="width: 100 px; height: 40px;">
-          </a>
-        </div>
-        <div class="navbar-menu-wrapper d-flex align-items-center justify-content-end">
-          <button class="navbar-toggler navbar-toggler align-self-center" type="button" data-toggle="minimize">
-            <span class="icon-menu"></span>
-          </button>
-          <ul class="navbar-nav mr-lg-2">
-            <li class="nav-item nav-search d-none d-lg-block">
-              <div class="input-group">
-                <div class="input-group-prepend hover-cursor" id="navbar-search-icon">
-                  <span class="input-group-text" id="search">
-                    <i class="icon-search"></i>
-                  </span>
-                </div>
-                <input type="text" class="form-control" id="navbar-search-input" placeholder="Search now" aria-label="search" aria-describedby="search">
+ <!-- partial:../../partials/_navbar.html -->
+ <nav class="navbar col-lg-12 col-12 p-0 fixed-top d-flex flex-row">
+                  <div class="text-center navbar-brand-wrapper d-flex align-items-center justify-content-start">
+                  <a class="navbar-brand brand-logo me-5" href="{{ url ('/penjual') }}" >
+                      <img src="{{ asset('themewagon/img/logo/logo_header.png') }}" alt="Logo Kabupaten Sleman" style="width: 110 px; height: 52px;">
+                    </a>
+                    <a class="navbar-brand brand-logo-mini" href="{{ url('/penjual') }}">
+                      <img src="{{ asset('themewagon/img/logo/logo kabupaten sleman.png') }}"  alt="Logo Kabupaten Sleman" style="width: 100 px; height: 40px;">
+                    </a>
+                  </div>
+                  <div class="navbar-menu-wrapper d-flex align-items-center justify-content-end">
+                    <button class="navbar-toggler navbar-toggler align-self-center" type="button" data-toggle="minimize">
+                      <span class="icon-menu"></span>
+                    </button>
+                    <!--<ul class="navbar-nav mr-lg-2">
+                    <li class="nav-item nav-search d-none d-lg-block">
+                      <div class="input-group">
+                        <div class="input-group-prepend hover-cursor" id="navbar-search-icon">
+                          <span class="input-group-text" id="search">
+                            <i class="icon-search"></i>
+                          </span>
+                        </div>
+                        <input type="text" class="form-control" id="navbar-search-input" placeholder="Search now" aria-label="search" aria-describedby="search">
+                      </div>
+                    </li>
+                  </ul>-->
+                    <ul class="navbar-nav navbar-nav-right">
+                    <div class="header-right1 d-flex align-items-center justify-content-center">
+              <!-- Social -->
+              <div class="header-social d-flex align-items-center">
+                  <!-- Icon Power -->
+                  <a class="nav-link d-flex align-items-center mx-3" href="#">
+                      <i class="ti-power-off text-primary" style="font-size: 24px; margin-right: 10px;"></i>
+                      <span style="font-size: 16px;">Logout</span>
+                  </a>
               </div>
-            </li>
+          </div>    
+              <li class="nav-item nav-settings d-none d-lg-flex">
+                  <a class="nav-link" href="#">
+                      <i class="mdi mdi-arrow-up-bold-circle-outline"></i>
+                  </a>
+              </li>
           </ul>
-          <ul class="navbar-nav navbar-nav-right">
-          <div class="header-right1 d-flex align-items-center justify-content-center">
-    <!-- Social -->
-    <div class="header-social d-flex align-items-center">
-        <!-- Icon Power -->
-        <a class="nav-link d-flex align-items-center mx-3" href="#">
-            <i class="ti-power-off text-primary" style="font-size: 24px; margin-right: 10px;"></i>
-            <span style="font-size: 16px;">Logout</span>
-        </a>
-    </div>
-</div>    
-    <li class="nav-item nav-settings d-none d-lg-flex">
-        <a class="nav-link" href="#">
-            <i class="mdi mdi-arrow-up-bold-circle-outline"></i>
-        </a>
-    </li>
-</ul>
 
-<button class="navbar-toggler navbar-toggler-right d-lg-none align-self-center" type="button" data-toggle="offcanvas">
-    <span class="icon-menu"></span>
-</button>
+          <button class="navbar-toggler navbar-toggler-right d-lg-none align-self-center" type="button" data-toggle="offcanvas">
+              <span class="icon-menu"></span>
+          </button>
 
-  </div>
-</nav>
-      <!-- partial -->
-      <div class="container-fluid page-body-wrapper">
-        <!-- partial:../../partials/_sidebar.html -->
-        <nav class="sidebar sidebar-offcanvas" id="sidebar">
-          <ul class="nav">
-            <li class="nav-item">
-              <a class="nav-link" href="{{ asset('/adminpreneur') }}">
-                <i class="icon-grid menu-icon"></i>
-                <span class="menu-title">Dashboard</span>
-              </a>
-          </li>
-            <li class="nav-item">
-              <a class="nav-link" href="{{ url('/kelolapreneur') }}">
-                <i class="mdi mdi-shape-plus menu-icon"></i>
-                <span class="menu-title">Kelola Produk</span>
-              </a>
-            </li>
-            <li class="nav-item">
-              <a class="nav-link" href="{{ url('/kelolahomepagepreneur') }}">
-                <i class="mdi mdi-home menu-icon"></i>
-                <span class="menu-title">Kelola Home Page</span>
-              </a>
-            </li>
-            <!--<li class="nav-item">
-              <a class="nav-link" href="{{ url('/laporanpreneur') }}">
-                <i class="icon-paper menu-icon"></i>
-                <span class="menu-title">Laporan Desa Preneur</span>
-              </a>
-            </li>-->
-        </nav>
-<!-- partial -->
+            </div>
+          </nav>
+          <!-- partial -->
+          <div class="container-fluid page-body-wrapper">
+            <!-- partial:../../partials/_sidebar.html -->
+            <nav class="sidebar sidebar-offcanvas" id="sidebar">
+              <ul class="nav">
+                <li class="nav-item">
+                  <a class="nav-link"  href="{{ url('/adminpreneur') }}">
+                    <i class="icon-grid menu-icon"></i>
+                    <span class="menu-title">Dashboard</span>
+                  </a>
+              </li>
+                <li class="nav-item">
+                  <a class="nav-link" href="{{ url('/kelolapreneur') }}">
+                    <i class="mdi mdi-shape-plus menu-icon"></i>
+                    <span class="menu-title">Kelola Produk</span>
+                  </a>
+                </li>
+                <li class="nav-item">
+                  <a class="nav-link" href="{{ url('/kelolahomepagepreneur') }}">
+                    <i class="mdi mdi-home menu-icon"></i>
+                    <span class="menu-title">Kelola Home Page</span>
+                  </a>
+                </li>
+                <!--<li class="nav-item">
+                  <a class="nav-link" href="{{ url('/laporanprima') }}">
+                    <i class="icon-paper menu-icon"></i>
+                    <span class="menu-title">Laporan Desa Prima</span>
+                  </a>
+                </li>-->
+            </nav>
+            <!-- partial -->
           <div class="main-panel">
           <div class="content-wrapper">
           <div class="row"></div>
@@ -121,7 +121,7 @@
               <div class="card-body">
                 <h4 class="card-title">Kelola Banner Desa Preneur</h4>
                 <div class="card-body">
-                  <form class="forms-sample" action="{{ url('/update-banner-preneur') }}" method="POST" enctype="multipart/form-data">
+                  <form class="forms-sample" action="{{ url('/updateBannerPreneur') }}" method="POST" enctype="multipart/form-data">
                       @csrf
 
                       <div class="form-group">
